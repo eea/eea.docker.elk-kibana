f9587af2faaddb02ef0698d0d0de2d8ec889a087
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2018-04-11 18:38:20 +0300

    Changed variable usage for elastic6-engine stack (#1)
    * Fixed variables
    
    * Updated docker-compose example

--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -8,8 +8,13 @@ fi
 
 cp /opt/kibana.yml /opt/kibana/config/kibana.yml
 
-echo "elasticsearch.url: 'https://elasticsearch:9200'" >> /opt/kibana/config/kibana.yml
-echo "server.host: '0.0.0.0'" >> /opt/kibana/config/kibana.yml
+
+ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-'https://elasticsearch:9200'}
+SERVER_HOST=${SERVER_HOST:-'0.0.0.0'}
+
+
+echo "elasticsearch.url: '$ELASTICSEARCH_URL'" >> /opt/kibana/config/kibana.yml
+echo "server.host: '$SERVER_HOST'" >> /opt/kibana/config/kibana.yml
 
 # if [ -z "$KIBANA_RW_USERNAME" ]           
 # then
@@ -23,7 +28,7 @@ echo "server.host: '0.0.0.0'" >> /opt/kibana/config/kibana.yml
 #self signed certificate in elasticsearch
 echo "elasticsearch.ssl.verificationMode: none" >> /opt/kibana/config/kibana.yml
 
-if [ $ENABLE_SSL = "YES" ]
+if [[ "$ENABLE_SSL" == "YES" ]]
 then
   mkdir -p /var/ssl
   rm -f /var/ssl/*
