ff5d44323bbb454c8f303f1bd6b9d303ba470efc
valentinab25 <valentinab25>
2020-04-23 22:27:40 +0300

    Upgrade to 6.8.8, xpack Refs #117009

--- kibana/inspire/docker-entrypoint.sh
+++ kibana/inspire/docker-entrypoint.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 set -e
 
-# Add kibana as command if needed
-if [[ "$1" == -* ]]; then
-	set -- kibana "$@"
-fi
 
+export ELASTICSEARCH_HOSTS=${ELASTICSEARCH_HOSTS:-$ELASTICSEARCH_URL}
+export ELASTICSEARCH_HOSTS=${ELASTICSEARCH_HOSTS:-'http://elastic:9200'}
+export ELASTICSEARCH_USERNAME=${ELASTICSEARCH_USERNAME:-$KIBANA_RW_USERNAME}
+export ELASTICSEARCH_PASSWORD=${ELASTICSEARCH_PASSWORD:-$KIBANA_RW_PASSWORD}
 
-ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-'https://elasticsearch:9200'}
-SERVER_HOST=${SERVER_HOST:-'0.0.0.0'}
+export ELASTICSEARCH_SSL_VERIFICATIONMONDE=${ELASTICSEARCH_SSL_VERIFICATIONMONDE:-none}
 
-
-echo "elasticsearch.hosts: '$ELASTICSEARCH_URL'" >> /opt/kibana/config/kibana.yml
-echo "server.host: '$SERVER_HOST'" >> /opt/kibana/config/kibana.yml
-
-if [ -n "$KIBANA_RW_USERNAME" ]           
+if [[ "$ENABLE_SSL" == "YES" ]] || [[ "$SERVER_SSL_ENABLED" == "true" ]]
 then
-  echo "elasticsearch.username: \"KIBANA_RW_USERNAME\"" >> /opt/kibana/config/kibana.yml
-  echo "elasticsearch.password: \"KIBANA_RW_PASSWORD\"" >> /opt/kibana/config/kibana.yml
-
-  sed "s#KIBANA_RW_USERNAME#$KIBANA_RW_USERNAME#g" -i /opt/kibana/config/kibana.yml
-  sed "s#KIBANA_RW_PASSWORD#$KIBANA_RW_PASSWORD#g" -i /opt/kibana/config/kibana.yml
-fi
-
-#self signed certificate in elasticsearch
-echo "elasticsearch.ssl.verificationMode: none" >> /opt/kibana/config/kibana.yml
-
-if [[ "$ENABLE_SSL" == "YES" ]]
-then
+  export SERVER_SSL_ENABLED='true'
+  export SERVER_SSL_KEY=${SERVER_SSL_KEY:-'/var/ssl/server.key'}
+  export SERVER_SSL_CERTIFICATE=${SERVER_SSL_CERTIFICATE:-'/var/ssl/server.crt'}
+  
   mkdir -p /var/ssl
   rm -f /var/ssl/*
   echo "creating ssl certificates"
@@ -35,26 +22,12 @@ then
   cat /ssl/server.crt > /var/ssl/server.crt  
   cat /ssl/server.key > /var/ssl/server.key
 
-  echo "server.ssl.enabled: true" >> /opt/kibana/config/kibana.yml
-  echo "server.ssl.key: /var/ssl/server.key" >> /opt/kibana/config/kibana.yml
-  echo "server.ssl.certificate: /var/ssl/server.crt" >> /opt/kibana/config/kibana.yml
-
   if [ -f /ssl/server-chain.crt ]; then
     cat /ssl/server-chain.crt > /var/ssl/server-chain.crt
-    echo 'server.ssl.certificate_authorities: "/var/ssl/server-chain.crt"' >> /opt/kibana/config/kibana.yml
+    export SERVER_SSL_CERTIFICATE_AUTHORITIES=${SERVER_SSL_CERTIFICATE_AUTHORITIES:-'/var/ssl/server-chain.crt'}
   fi
 
   chmod 400 /var/ssl/*
 
 fi
-
-# Run as user "kibana" if the command is "kibana"
-if [ "$1" = 'kibana' ]; then
-	#if [ "$ELASTICSEARCH_URL" ]; then
-	#	sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
-	#fi
-	
-	set -- gosu kibana tini -- "$@"
-fi
-
 exec "$@"
