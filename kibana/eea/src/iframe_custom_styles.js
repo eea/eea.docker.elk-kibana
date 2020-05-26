de1cec333c05988a67ef349e4ef8700446c8a9cd
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2020-05-26 17:46:44 +0300

    fixed typos

--- kibana/eea/src/iframe_custom_styles.js
+++ kibana/eea/src/iframe_custom_styles.js
@@ -3,6 +3,6 @@ if (window.self !== window.top){
 
   document.head.appendChild(styleEl);
 
-  styleEl.sheet.insertRule('.content .app-wrapper{top:20px!important;}')
+  styleEl.sheet.insertRule('.content .app-wrapper{top:0!important;}')
   styleEl.sheet.insertRule('html{background-color:white;}')
 }
