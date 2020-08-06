0b31c06d1b518eecae2fb30dd7a0a7d30c0355ce
Mauro Michielon - EEA 06Oct2015 <michimau@kvm-dev5-06.pdmz.eea>
2020-08-06 19:45:23 +0200

    inspire

--- kibana/eea/src/iframe_custom_styles.js
+++
@@ -1,8 +0,0 @@
-if (window.self !== window.top){
-  var styleEl = document.createElement('style');
-
-  document.head.appendChild(styleEl);
-
-  styleEl.sheet.insertRule('.content .app-wrapper{top:0!important;}')
-  styleEl.sheet.insertRule('html{background-color:white;}')
-}
