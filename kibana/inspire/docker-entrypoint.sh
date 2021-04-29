c50a60a7c23178cf5b5b0f8cc6144b0508632662
valentinab25 <valentinab25>
2021-04-29 16:15:52 +0300

    Fix inspire version

--- kibana/inspire/docker-entrypoint.sh
+++ kibana/inspire/docker-entrypoint.sh
@@ -52,4 +52,50 @@ cat template_head > $file
 echo "const logo = _react.default.createElement(\"img\", {src:'https://raw.githubusercontent.com/eea/eea.docker.elk-kibana/7.8.1/kibana/inspire/src/app.ico',width:'80px'});" >> $file
 cat template_tail >> $file
 
-exec "$@"
+#password enabled and anonimous access enabled
+if [[ "${ALLOW_ANON_RO}" == "true" ]] && [ ! -f /tmp/users_created ] && [ -n "$elastic_password" ]; then
+
+  #setting variables used in configuration, using default values
+  anon_password=$(openssl rand -base64 12)
+  ANON_PASSWORD="${ANON_PASSWORD:-$anon_password}"
+  read_only_role_json='{"elasticsearch":{"cluster":["monitor"],"indices":[{"names":["*"],"privileges":["read","view_index_metadata"]},{"names":[".kibana"],"privileges":["read","view_index_metadata"],"field_security":{"grant":["*"]}}],"run_as":[]},"kibana":[{"spaces":["*"],"base":["read"],"feature":{}}]}'
+  READ_ONLY_ROLE_JSON="${READ_ONLY_ROLE_JSON:-$read_only_role_json}"
+
+  echo "Adding anonimous access to kibana.yml"
+  echo "
+xpack.security.authc.providers:
+  basic.basic1:
+    order: 0
+    description: \"Log in to have edit rights\"
+  anonymous.anonymous1:
+    order: 1
+    description: \"Continue as guest\"
+    icon: \"globe\"
+    credentials:
+      username: \"anonymous_service_account\"
+      password: \"${ANON_PASSWORD}\"
+" >> config/kibana.yml
+
+  "$@" &
+
+  #wait for the kibana user interface to be up
+  while [ $( curl -I -s  -uelastic:$elastic_password  localhost:5601/internal/security/users/elastic | grep -c 200 )  -eq 0 ]; do sleep 10; done
+
+
+  if  [ $( curl -I -s -uelastic:$elastic_password  localhost:5601/api/security/role/read_only | grep -ic "200 OK" ) -eq 0 ]; then
+     echo "Setting default read_only role"
+     curl  -uelastic:$elastic_password -X PUT -H 'Content-Type: application/json' -H "kbn-xsrf: reporting" localhost:5601/api/security/role/read_only -d"$READ_ONLY_ROLE_JSON"
+  fi
+
+  echo "Setting default anonymous_service_account user"
+  curl  -uelastic:$elastic_password -X POST -H 'Content-Type: application/json'   -H "kbn-xsrf: reporting" localhost:5601/internal/security/users/anonymous_service_account -d"{\"password\":\"$ANON_PASSWORD\",\"username\":\"anonymous_service_account\",\"full_name\":\"\",\"email\":\"\",\"roles\":[\"read_only\"]}"
+  touch /tmp/users_created
+
+  wait
+
+else
+
+  exec "$@"
+
+fi
+
